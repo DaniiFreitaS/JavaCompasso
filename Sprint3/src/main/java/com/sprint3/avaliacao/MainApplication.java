1c6bef71b29948f45bf2a1e3ecab19866bf8ad0c
Daniel Felipe Coelho de Freitas <danielfreitaslele14@gmail.com>
2021-12-11 21:23:43 -0300

    iniciando projeto

---
+++ Sprint3/src/main/java/com/sprint3/avaliacao/MainApplication.java
@@ -0,0 +1,13 @@
+package com.sprint3.avaliacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+}
---
+++ Sprint3/src/test/java/com/sprint3/avaliacao/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprint3.avaliacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
