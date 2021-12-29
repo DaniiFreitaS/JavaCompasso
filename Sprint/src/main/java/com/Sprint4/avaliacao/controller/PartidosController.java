4dd1e4c005c8a137e58c44b0318da0c759ad9405
Daniel Felipe Coelho de Freitas <danielfreitaslele14@gmail.com>
2021-12-29 18:32:57 -0300

    adcionando controller

--- Sprint/src/main/java/com/Sprint4/avaliacao/controller/PartidosController.java
+++ Sprint/src/main/java/com/Sprint4/avaliacao/controller/PartidosController.java
@@ -1,4 +1,31 @@
 package com.Sprint4.avaliacao.controller;
 
+
+import com.Sprint4.avaliacao.controller.dto.PartidosDto;
+import com.Sprint4.avaliacao.partidos.Partidos;
+import com.Sprint4.avaliacao.repository.PartidosRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/partidos")
 public class PartidosController {
+    @Autowired
+    private PartidosRepository partidosRepository;
+
+    @GetMapping
+    public List<PartidosDto> lista(){
+        List<Partidos> partidos = null;
+
+        partidos = partidosRepository.findAll();
+
+        return PartidosDto.converter(partidos);
+
+
+    }
+
 }
